1c4bb381270f16449889d46f95347e5d434b987c
Dinara <assylkhanova@gmail.com>
2023-08-14 08:49:14 +0200

    Add tests and update README

---
+++ tests/test_etl.py
@@ -0,0 +1,18 @@
+import sys
+sys.path.append('../scripts')  # Append path to the scripts directory
+
+import unittest
+from etl import extract_main_dataframes
+
+class TestETL(unittest.TestCase):
+
+    def test_extract_main_dataframes(self):
+        main_dfs = extract_main_dataframes(start_year=1995, end_year=1999)
+        #TODO: expand to all dataframes
+        test_df = main_dfs[list(main_dfs.keys())[0]]
+        
+        expected_columns = ['Country Name', 'Country Code', 'Indicator Name', 'Indicator Code', '1995', '1996', '1997', '1998', '1999']
+        self.assertListEqual(test_df.columns.tolist(), expected_columns)
+
+if __name__ == '__main__':
+    unittest.main()
